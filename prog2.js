952d30797b26741d30f2a7afb7fd5c95dbc9ea09
MAYCON GUIMARAES PAULINO <maycon.paulino@aluno.senac.rio>
2026-03-11 20:36:10 -0300

    prog2

---
+++ prog2.js
@@ -0,0 +1,6 @@
+let valor = 8;
+console.log("O conteudo do valor é: " + valor);
+
+/*
+Agora adicione este arquivo ao seu git depois faça um commit sinalizando o que você fez envie para o github
+*/
